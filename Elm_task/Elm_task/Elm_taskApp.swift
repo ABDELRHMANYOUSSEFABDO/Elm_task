b799fd92ab4d34ce1594f4e4f97747f8603c32c4
Abdelrahman.Youssef <abderlrhmanyoussef@gmail.com>
2024-09-21 14:58:10 +0300

    initia App

---
+++ Elm_task/Elm_task/Elm_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elm_taskApp.swift
+//  Elm_task
+//
+//  Created by Abdelrahman.Youssef on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Elm_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
